0bbbeab934da64412a122115a8ef37d729b2daff
Mahmoud Bendary <bendarymody@gmail.com>
2025-04-19 06:34:29 +0200

    Initial commit

---
+++ config-server/src/test/java/com/TPM/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TPM.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
